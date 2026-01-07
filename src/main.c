48a9b5ac0e9b238f5ff2363ddf88949de5c04283
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-01-07 21:26:01 +0100

    Add helper to initialize and free the table

--- src/main.c
+++ src/main.c
@@ -198,6 +198,25 @@ void *row_slot(Table *table, uint32_t row_num)
     return page + byte_offset; // memory pointer
 }
 
+Table *new_table(void)
+{
+    Table *table = (Table*)malloc(sizeof(Table));
+    table->num_rows = 0;
+  
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table *table)
+{
+    for (int i = 0; i < table->pages[i]; i++) {
+      free(table->pages[i]);
+    }
+    free(table);
+}
+
 // REPL
 int main(int argc, char *argv[])
 {
