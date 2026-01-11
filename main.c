f351949e20a16b0b1431baadba09acb34a7c2e57
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-01-11 20:43:45 +0100

    Implement cache miss with get_page

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <stdint.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
 
 // A small wrapper around the state we need to store to interact with getline()
@@ -162,17 +163,48 @@ static void print_row(Row *row)
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
+// The logic for handling a cache miss
+// Page 0 at offset 0, page 1 at offset 4096, page 2 at offset 8192, etc
+void *get_page(Pager *pager, uint32_t page_num)
+{
+  if (page_num > TABLE_MAX_PAGES) {
+    printf("Tried to fetch page number out of bounds. %d > %d\n", page_num,
+        TABLE_MAX_PAGES);
+    exit(EXIT_FAILURE);
+  }
+  if (pager->pages[page_num] == NULL) {
+    // cache miss. allocate memory and load from file.
+    void *page = malloc(PAGE_SIZE);
+    if (page == NULL) exit(EXIT_FAILURE);
+    uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+    // We might save a partial page at the end of the file
+    if (pager->file_length % PAGE_SIZE) {
+      num_pages += 1;
+    }
+
+    if (page_num <= num_pages) {
+      lseek(pager->fd, page_num * PAGE_SIZE, SEEK_SET);
+      ssize_t bytes_read = read(pager->fd, page, PAGE_SIZE);
+      if (bytes_read == -1) {
+        printf("Error reading file: %d\n", errno);
+        exit(EXIT_FAILURE);
+      }
+    }
+    pager->pages[page_num] = page;
+  }
+  return pager->pages[page_num];
+}
+
+
 // Helper to read/write address memory for a particular row 
 void *row_slot(Table *table, uint32_t row_num)
 {
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = table->pages[page_num];
-    if (page == NULL) {
-        // Lazy allocation
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
-    }
+    void *page = get_page(table->pager, page_num);   
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
+
     return page + byte_offset; // memory pointer
 }
 
