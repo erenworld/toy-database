6f1eadb945b271816d3d78fbe5d41022aceb4c87
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-01-11 21:02:11 +0100

    Add db_close for flushes the page cache to disk and close the db file

--- main.c
+++ main.c
@@ -196,7 +196,6 @@ void *get_page(Pager *pager, uint32_t page_num)
   return pager->pages[page_num];
 }
 
-
 // Helper to read/write address memory for a particular row 
 void *row_slot(Table *table, uint32_t row_num)
 {
@@ -286,6 +285,49 @@ ExecuteResult execute_statement(Statement *statement, Table *table)
     case (STATEMENT_SELECT):
       return execute_select(statement, table);
   }
+}
+
+void *db_close(Table *table)
+{
+  Pager *pager = table->pager;
+  uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+  for (uint32_t i = 0; i < num_full_pages; i++) {
+    if (pager->pages[i] == NULL) {
+      continue;
+    }
+    pager_flush(pager, i, PAGE_SIZE);
+    free(pager->pages[i]);
+    pager->pages[i] = NULL;
+  }
+  // There may be a partial page to write to the end of the file
+  // This should not be needed after we switch to a B-tree
+  uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+  if (num_additional_rows > 0) {
+    uint32_t page_num = num_full_pages;
+
+    if (pager->pages[page_num] != NULL) {
+      pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+      free(pager->pages[page_num]);
+      pager->pages[page_num] = NULL;
+    }
+  }
+
+  int result = close(pager->fd);
+  if (result == -1) {
+    printf("Error closing db file.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+    void *page = pager->pages[i];
+    if (page) {
+      free(page);
+      pager->pages[i] = NULL;
+    }
+  }
+  free(pager);
+  free(table);
 }
 
 Table *db_open(const char *filename)
