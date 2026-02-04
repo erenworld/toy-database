4e36ccf6c56a342215f929c99c519052de407bdc
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-02-04 14:55:23 +0100

    Changes to the Cursor object

--- main.c
+++ main.c
@@ -72,6 +72,68 @@ const uint32_t PAGE_SIZE = 4096;
 #define TABLE_MAX_PAGES 100
 
 
+// BTREE
+typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
+
+// Node header layout
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t); // 1
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t); // 4
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE =
+    NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE; // 6
+
+
+// Leaf Node header layout
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE =
+    COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE; // 10
+
+// Leaf Node body layout
+// An array of cells. Each cell is a key followed by a value (a serialized row)
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET =
+    LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS =
+    LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+
+
+// access keys, values and metadata
+
+uint32_t *leaf_node_num_cells(void *node)
+{
+  return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+void *leaf_node_cell(void *node, uint32_t cell_num)
+{
+  return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+}
+
+uint32_t *leaf_node_key(void *node, uint32_t cell_num)
+{
+  return leaf_node_cell(node, cell_num);
+}
+
+void *leaf_node_value(void *node, uint32_t cell_num)
+{
+  return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void *node) 
+{
+  *leaf_node_num_cells(node) = 0;
+}
+
+
 typedef struct {
   int fd;
   uint32_t file_length;
@@ -86,7 +148,8 @@ typedef struct {
 
 typedef struct {
     Table *table;
-    uint32_t row_num;
+    uint32_t page_num;
+    uint32_t cell_num;
     bool end_of_table;  // Indicates a position one past the last element
 } Cursor;
 
@@ -253,7 +316,7 @@ void *get_page(Pager *pager, uint32_t page_num)
     if (page == NULL) exit(EXIT_FAILURE);
     uint32_t num_pages = pager->file_length / PAGE_SIZE;
 
-    // We might save a partial page at the end of the file
+    // I might save a partial page at the end of the file
     if (pager->file_length % PAGE_SIZE) {
       num_pages += 1;
     }
@@ -419,7 +482,7 @@ Table *db_open(const char *filename)
 
     Table *table = malloc(sizeof(Table));
     table->pager = pager;
-    table->num_rows = num_rows;  
+    // table->num_rows = num_rows;  
   
     return table;
 }
