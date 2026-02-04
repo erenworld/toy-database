6170e2fe137d3749925874638bf9841f22c2794c
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-02-04 15:35:42 +0100

    Add new tests for constants

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -105,4 +105,22 @@
             "sqlite > ",
       ])
     end
+
+    it 'prints constants' do
+      script = [
+        ".constants",
+        ".exit",
+      ]
+      result = run_script(script)
+      expect(result).to match_array([
+        "db > Constants:",
+        "ROW SIZE: 293",
+        "ROW_SIZE: 293",
+        "COMMON_NODE_HEADER_SIZE: 6",
+        "LEAF_NODE_HEADER_SIZE: 10",
+        "LEAF_NODE_CELL_SIZE: 297",
+        "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+        "LEAF_NODE_MAX_CELLS: 13",
+        "db > ",
+      ])g
   end
