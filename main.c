8f1b307c664431ebcb99dc634edd529e1d64c40c
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-02-05 14:29:10 +0100

    Add sorted order in test

--- main.c
+++ main.c
@@ -26,8 +26,8 @@ typedef struct {
 
 typedef enum { 
   EXECUTE_SUCCESS, 
-  EXECUTE_TABLE_FULL, 
-  EXECUTE_DUPLICATE_KEY 
+  EXECUTE_DUPLICATE_KEY,
+  EXECUTE_TABLE_FULL
 } ExecuteResult;
 
 typedef enum 
@@ -157,8 +157,65 @@ void *leaf_node_value(void *node, uint32_t cell_num)
   return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
+
+Cursor *leaf_node_find(Table *table, uint32_t page_num, uint32_t key)
+{
+  void *node = sqlitePagerGet(table->pager, page_num);
+  uint32_t num_cells = *btreeLeafCount(node);
+
+  Cursor *cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->page_num = page_num;
+
+  // binary search
+  uint32_t min_index = 0;
+  uint32_t one_past_max_index = num_cells;
+  while (one_past_max_index != min_index) {
+    uint32_t index = (min_index + one_past_max_index) / 2;
+    uint32_t key_at_index = *btreeLeafKey(node, index);
+    if (key == key_at_index) {
+      cursor->cell_num = index;
+      return cursor;
+    }
+    if (key < key_at_index) {
+      one_past_max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+  cursor->cell_num = min_index;
+  return cursor;
+}
+
+NodeType sqliteNodeType(void *node)
+{
+  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+  return (NodeType)value;
+}
+
+void sqliteSetNodeType(void *node, NodeType type)
+{
+  uint8_t value = type;
+  *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
+// Return the position of the given key/ If the key is not present, return the position where it should be inserted
+Cursor *sqliteBtreeSearch(Table *table, uint32_t key)
+{
+  uint32_t root_page_num = table->root_page_num;
+  void *root_node = sqlitePagerGet(table->pager, root_page_num);
+
+  if (sqliteNodeType(root_node) == NODE_LEAF) {
+    return leaf_node_find(table, root_page_num, key);
+  } else {
+    printf("Need to implement searching an internal node\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
 void btreeInitLeafNode(void *node) 
 {
+  sqliteSetNodeType(node, NODE_LEAF);
   *btreeLeafCount(node) = 0;
 }
 
@@ -439,61 +496,6 @@ PrepareResult prepare_statement(InputBuffer *input, Statement *statement)
   return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-Cursor *leaf_node_find(Table *table, uint32_t page_num, uint32_t key)
-{
-  void *node = sqlitePagerGet(table->pager, page_num);
-  uint32_t num_cells = *btreeLeafCount(node);
-
-  Cursor *cursor = malloc(sizeof(Cursor));
-  cursor->table = table;
-  cursor->page_num = page_num;
-
-  // binary search
-  uint32_t min_index = 0;
-  uint32_t one_past_max_index = num_cells;
-  while (one_past_max_index != min_index) {
-    uint32_t index = (min_index + one_past_max_index) / 2;
-    uint32_t key_at_index = *btreeLeafKey(node, index);
-    if (key == key_at_index) {
-      cursor->cell_num = index;
-      return cursor;
-    }
-    if (key < key_at_index) {
-      one_past_max_index = index;
-    } else {
-      min_index = index + 1;
-    }
-  }
-  cursor->cell_num = min_index;
-  return cursor;
-}
-
-NodeType sqliteNodeType(void *node)
-{
-  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
-  return (NodeType)value;
-}
-
-void sqliteSetNodeType(void *node, NodeType type)
-{
-  uint8_t value = type;
-  *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
-}
-
-// Return the position of the given key/ If the key is not present, return the position where it should be inserted
-Cursor *sqliteBtreeSearch(Table *table, uint32_t key)
-{
-  uint32_t root_page_num = table->root_page_num;
-  void *root_node = sqlitePagerGet(table->pager, root_page_num);
-
-  if (sqliteNodeType(root_node) == NODE_LEAF) {
-    return leaf_node_find(table, root_page_num, key);
-  } else {
-    printf("Need to implement searching an internal node\n");
-    exit(EXIT_FAILURE);
-  }
-}
-
 ExecuteResult execute_insert(Statement *statement, Table *table)
 {
   void *node = sqlitePagerGet(table->pager, table->root_page_num);
@@ -646,6 +648,9 @@ int main(int argc, char *argv[])
     switch (execute_statement(&statement, table)) {
       case (EXECUTE_SUCCESS):
         printf("Executed.\n");
+        break;
+      case (EXECUTE_DUPLICATE_KEY):
+        printf("Error: Duplicate key.\n");
         break;
       case (EXECUTE_TABLE_FULL):
         printf("Error: table full.\n");
